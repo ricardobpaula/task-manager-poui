4221829634528cf4d0b7e6f8b5174c32c05f7d50
Ricardo de Paula <flashdepaula@gmail.com>
2023-01-26 14:34:57 -0300

    start config project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PoMenuItem } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task-manager-poui';
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Tarefas', link: '#', icon: 'po-icon po-icon-document', }
+  ]
 }
